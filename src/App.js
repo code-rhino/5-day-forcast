e912137eaab901538c28db749146211d4b799d27
henryonyedum <78107417+henryonyedum@users.noreply.github.com>
2021-10-18 16:35:34 -0400

    added api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
-
+fetch ('api.openweathermap.org/data/2.5/forecast?q=London,us&appid=03391c8ecd63478e020be2d29b18101e')
+.then(response => response.json())
+.then(data => console.log(data))
 export default App;
